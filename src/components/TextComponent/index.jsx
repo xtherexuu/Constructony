f62f0ef0b8c5c96de3e1d91bf7e8629af65d8620
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2024-05-07 17:16:58 +0200

    added align property to TextComponent

--- src/components/TextComponent/index.jsx
+++ src/components/TextComponent/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyledText } from "./styled";
 
-export default function TextComponent({ children }) {
-  return <StyledText>{children}</StyledText>;
+export default function TextComponent({ children, align }) {
+  return <StyledText align={align}>{children}</StyledText>;
 }
